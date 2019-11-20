34f036dbfd2de167ef91d6102638feda15ee6842
Francisco Diaz <francisco.d@47deg.com>
2019-11-20 14:15:35 +0000

    Creates deploy bash script

---
+++ deploy.sh
@@ -0,0 +1,27 @@
+  
+#!/usr/bin/env bash
+
+cd try.arrow-kt.io
+
+git fetch --prune
+
+git checkout master
+
+git pull
+
+cp ~/cert/* -r docker/frontend/conf
+
+sh gradlew clean
+
+sh gradlew ::copyKotlinLibs
+
+sh gradlew war
+
+sudo docker-compose down
+
+sudo docker system prune -a -f
+
+sudo docker volume rm $(sudo docker volume ls -qf dangling=true)
+
+sudo docker-compose up
+
